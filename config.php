d5d8ad7da6b6ea9201f31428d12d42258efc5c4c
Aditya Prabhu <59806748+AdityaPrabhuCS@users.noreply.github.com>
2022-01-28 18:47:00 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
 
 	// Attempt to connect to MySQL database
 	$mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-<
+
 	if (!$mysql_db) {
 		die("Error: Unable to connect " . $mysql_db->connect_error);
 	}
